a91425f079ee191b11f5df08142d0f64ab44baff
Rajat Gupta <dguptarajat@gmail.com>
2022-11-24 19:59:19 +0530

    feat - admin portal link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
-import { Home} from "./pages/home/Home";
+import { Home } from "./pages/home/Home";
+import "./App.css";
 
 export const App = () => {
-  return(
-    <Routes>
-      <Route path='/' element={<Home/>} />
-    </Routes>
-  )
-}
-
-
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
+};
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,16 +1,14 @@
-import style from Home.module.css
+import style from "./home.module.css";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
-    return(
-    <><div className={style.loginType}>
-            Admin Login
-        </div>
-        <div className={style.loginType}>
-            Staff Login
-        </div>
-        <div className={style.loginType}>
-            Student Login
-        </div>
-    </>  
-    )
-} 
+  return (
+    <main className={style.home}>
+      <a href="http://127.0.0.1:8000/admin/" className={style.loginType}>
+        Admin Login
+      </a>
+      <div className={style.loginType}>Staff Login</div>
+      <div className={style.loginType}>Student Login</div>
+    </main>
+  );
+};
